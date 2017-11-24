19f1ff5963a0677ba937a4c8944d3335bf713c92
Max Reuter <max.a.reuter@gmail.com>
2017-11-25 00:41:41 +0200

    load top artists from spotify

--- deejay-react/src/App.js
+++ deejay-react/src/App.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dotenv from 'dotenv';
+import 'whatwg-fetch';
+dotenv.config();
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      artists: []
+    }
+  }
+
+  getTopArtists() {
+    return fetch(`https://api.spotify.com/v1/me/top/artists`, {
+      method: 'get',
+      headers: { 'Authorization': `Bearer ${process.env.REACT_APP_MY_TOKEN}` }
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      return json.items.map(artist => {
+        return <li key={ artist.uri }><a href={ artist.uri }>{ artist.name }</a></li>;
+      });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+
   render() {
+    if(this.state.artists.length === 0) {
+      this.getTopArtists()
+      .then(res => {
+        this.setState({
+          ...this.state,
+          artists: res
+        })
+      });
+    }
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,8 +51,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          My favourite artists are:
         </p>
+        <ul>{ this.state.artists }</ul>
       </div>
     );
   }
