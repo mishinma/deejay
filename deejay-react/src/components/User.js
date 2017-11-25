ad8521b2ce0e1656576d82ed32e9b30bb73424db
Max Reuter <max.a.reuter@gmail.com>
2017-11-25 19:38:21 +0200

    fix custom urls

--- deejay-react/src/components/User.js
+++ deejay-react/src/components/User.js
@@ -4,11 +4,12 @@ import default_image from '../Spotify_Icon_RGB_Green.png';
 
 const User = (props) => {
     const src = props.user.images.length > 0
-        ? props.user.images[0]
+        ? props.user.images[0].url
         : default_image;
     const name = props.user.display_name
         ? props.user.display_name
         : props.user.id;
+    console.log(props.user);
     return (
         <div>
             <Image src={src} style={{ width: '75px' }} responsive circle />
