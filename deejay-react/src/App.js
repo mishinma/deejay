23642a9aa1e5af691d775df77ea1e3f75fd8292a
Mikhail Mishin <mishinma1805@gmail.com>
2017-11-26 07:18:16 +0200

    get username

--- deejay-react/src/App.js
+++ deejay-react/src/App.js
@@ -12,18 +12,19 @@ class App extends Component {
 
     let content;
     const authEndpoint = 'https://accounts.spotify.com/authorize';
+    const guestScopes = ['user-read-private', 'user-read-email', 'user-read-birthdate', 'user-top-read'];
 
 
     // If there is no token, redirect to Spotify authorization
     if (this.props.roomUrl) {
-        const scopes = ['user-top-read'];
+        const scopes = guestScopes;
         console.log(this.props.roomUrl);
         content = <Join  authEndpoint={authEndpoint} token={this.props.token}
         roomUrl={this.props.roomUrl} scopes={scopes} callback={process.env.REACT_APP_CALLBACK_URL} />
     } else if (this.props.token) {
       content = <Room token={this.props.token} />
     } else {
-      const scopes = ['streaming', 'user-modify-playback-state', 'user-read-birthdate', 'user-read-email', 'user-read-private', 'user-top-read'];
+      const scopes = guestScopes.concat(['streaming', 'user-modify-playback-state']);
       content = <Home authEndpoint={authEndpoint} scopes={scopes} callback={process.env.REACT_APP_CALLBACK_URL} />
     }
 
